10cc0a206299ec84bf0ec2859ae6e820efce615f
mvsanghvi <mevsanghvi@gmail.com>
2025-03-12 23:59:26 -0500

    Edit typos

--- Map_Convert_Count.sh
+++ Map_Convert_Count.sh
@@ -1,19 +1,30 @@
-#Map Reads using HISAT2
+#!/bin/bash
+
+
+# Map Reads using HISAT2
 # Convert from SAM to BAM
 
-listD = ("WT_P62_1" "WT_P62_2" "Y2_P37_28_1" "Y2_P37_28_2" "YT2_P37_7_17_1" "YT2_P37_7_17_2")
-for i in listD:
-do
-    echo $i
-    file_1=$"$i"_1.fq.gz
-    file_2=$"$i"_2.fq.gz
-    hfile_sam=$"$i"_.sam
-    hisat2 -q --dta -p 8 -x grch38/genome -1 ./WT_P62_1.fq.gz -2 ./WT_P62_2.fq.gz  -S WT_P62.sam  -t
-    hfile_bam=$"$i"_h.bam
-    samtools sort -@ 8 -o hfile_bam hfile_sam
+listD=("WT_P62" "Y2_P37_28" "YT2_P37_7_17")
 
-    rm "$hfile_sam"
 
-    yt_folder= $"$i"_all
-    stringtie -e -B -p 8 -G grch38/genome ./$hfile_bam -o ./yt_all_gtf/$yt_folder/output_merge.gtf
+for i in "${listD[@]}"; do
+   echo "$i"
+  
+   file_1="${i}_1.fq.gz"
+   file_2="${i}_2.fq.gz"
+   hfile_sam="${i}.sam"
+  
+   hisat2 -q --dta -p 8 -x grch38/genome -1 "./$file_1" -2 "./$file_2" -S "$hfile_sam" -t
+  
+   hfile_bam="${i}_h.bam"
+  
+   samtools sort -@ 8 -o "$hfile_bam" "$hfile_sam"
+
+
+   rm "$hfile_sam"
+
+
+   yt_folder=$"$i"_all
+   mkdir -p ./yt_all_gtf/$yt_folder
+   stringtie -e -B -p 8 -G Homo_sapiens.GRCh38.110.gtf ./$hfile_bam -o ./yt_all_gtf/$yt_folder/output_merge.gtf
 done
