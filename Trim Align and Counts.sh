586153f8c74c0ce630530a2ee59b4fad32d3d632
mvsanghvi <mevsanghvi@gmail.com>
2025-04-05 17:06:33 -0500

    Reorder due to storage issues

--- Trim Align and Counts.sh
+++ Trim Align and Counts.sh
@@ -3,19 +3,6 @@
 ./raw/WT_P62/WT_P62_2.fq.gz \
 -o ./trimmed/WT_P62
 
-
-./TrimGalore-0.6.10/trim_galore --fastqc --paired --cores 8 \
-./raw/Y2_P37_28/Y2_P37_28_1.fq.gz \
-./raw/Y2_P37_28/Y2_P37_28_2.fq.gz \
--o ./trimmed/Y2_P37_28
-
-
-./TrimGalore-0.6.10/trim_galore --fastqc --paired --cores 8 \
-./raw/YT2_P37_7_17/YT2_P37_7_17_1.fq.gz \
-./raw/YT2_P37_7_17/YT2_P37_7_17_2.fq.gz \
--o ./trimmed/YT2_P37_7_17
-
-
 ./STAR/source/STAR --genomeDir ./star_index \
  --runThreadN 20 --readFilesIn \
  ./trimmed/WT_P62/WT_P62_1_val_1.fq.gz \
@@ -26,6 +13,17 @@
  --readFilesCommand zcat \
  --outFileNamePrefix ./aligned/WT_P62/
 
+featureCounts -T 8 -t exon -g gene_name -s 2 -p --countReadPairs \
+-a ./gtf/gencode.v47.annotation.gtf \
+-o ./aligned/WT_P62/featureCounts_gene.txt \
+./aligned/WT_P62/Aligned.sortedByCoord.out.bam
+
+#Download featureCounts and delete everything else to make room for next batch
+
+./TrimGalore-0.6.10/trim_galore --fastqc --paired --cores 8 \
+./raw/Y2_P37_28/Y2_P37_28_1.fq.gz \
+./raw/Y2_P37_28/Y2_P37_28_2.fq.gz \
+-o ./trimmed/Y2_P37_28
 
 ./STAR/source/STAR --genomeDir ./star_index \
  --runThreadN 20 --readFilesIn \
@@ -36,6 +34,18 @@
  --outSAMattributes Standard \
  --readFilesCommand zcat \
  --outFileNamePrefix ./aligned/Y2_P37_28/
+
+featureCounts -T 8 -t exon -g gene_name -s 2 -p --countReadPairs \
+-a ./gtf/gencode.v47.annotation.gtf \
+-o ./aligned/Y2_P37_28/featureCounts_gene.txt \
+./aligned/Y2_P37_28/Aligned.sortedByCoord.out.bam
+
+#Download featureCounts and delete everything else to make room for next batch
+
+./TrimGalore-0.6.10/trim_galore --fastqc --paired --cores 8 \
+./raw/YT2_P37_7_17/YT2_P37_7_17_1.fq.gz \
+./raw/YT2_P37_7_17/YT2_P37_7_17_2.fq.gz \
+-o ./trimmed/YT2_P37_7_17
 
 
 ./STAR/source/STAR --genomeDir ./star_index \
@@ -48,20 +58,9 @@
  --readFilesCommand zcat \
  --outFileNamePrefix ./aligned/YT2_P37_7_17/
 
-
-featureCounts -T 8 -t exon -g gene_name -s 2 -p --countReadPairs \
--a ./gtf/gencode.v47.annotation.gtf \
--o ./aligned/WT_P62/featureCounts_gene.txt \
-./aligned/WT_P62/Aligned.sortedByCoord.out.bam
-
-
-featureCounts -T 8 -t exon -g gene_name -s 2 -p --countReadPairs \
--a ./gtf/gencode.v47.annotation.gtf \
--o ./aligned/Y2_P37_28/featureCounts_gene.txt \
-./aligned/Y2_P37_28/Aligned.sortedByCoord.out.bam
-
-
 featureCounts -T 8 -t exon -g gene_name -s 2 -p --countReadPairs \
 -a ./gtf/gencode.v47.annotation.gtf \
 -o ./aligned/YT2_P37_7_17/featureCounts_gene.txt \
 ./aligned/YT2_P37_7_17/Aligned.sortedByCoord.out.bam
+
+#Download featureCounts and delete everything else to make room for next batch
