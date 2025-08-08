e3304f56f04ca2d5982a649bae52536d40fe185a
mvsanghvi <mevsanghvi@gmail.com>
2025-08-08 10:37:32 -0500

    Update

---
+++ RNAseq/Rep analysis/DEGs_w_reps.R
@@ -0,0 +1,37 @@
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install(c("sva", "edgeR", "limma", "Biobase", "biomaRt", 
+                       "clusterProfiler", "EnhancedVolcano", "org.Hs.eg.db", 
+                       "org.Mm.eg.db", "org.Rn.eg.db"))
+
+install.packages(c("data.table", "readxl", "stringr", "ggplot2", "ggrepel", 
+                   "ggfortify", "ggprism", "pheatmap", "VennDiagram", 
+                   "corrplot", "Hmisc", "stats", "tidyverse"))
+library(data.table)
+library(edgeR)
+library(limma)
+library(ggplot2)
+library(ggrepel)
+library(ggfortify)
+library(stats)
+library(sva)
+setwd("C:\Users\mvsan\code\YAP_TAZ_bulkRNAseq\RNAseq\Rep analysis")
+
+#Combine count files into single expression matrix
+# Retrieve count file paths
+file_names <- list.files(count_file_path, pattern = "featureCounts_exon\\.txt$", recursive = T, full.names = T)
+
+# Read all count files into a DGElist
+dgls <- readDGE(file_names, columns = c(1, 7), skip = 1)
+counts_raw <- as.data.frame(dgls$counts)
+
+# Rename samples
+sample_name <- gsub("_featureCounts_exon", "", basename(colnames(counts_raw)))
+colnames(counts_raw) <- sample_name
+
+# Save the combined count table
+fwrite(counts_raw, "counts_raw.tsv", sep = "\t")
+
+#Reading raw count data
+count_tbl <- fread("counts_raw.tsv", data.table = F)
