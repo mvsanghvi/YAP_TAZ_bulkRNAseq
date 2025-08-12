19b7b12a5adc692d872e358afeb47b6807498faf
mvsanghvi <mevsanghvi@gmail.com>
2025-08-11 20:55:25 -0500

    Clean data

--- RNAseq/Rep analysis/DEGs_w_reps.R
+++ RNAseq/Rep analysis/DEGs_w_reps.R
@@ -9,18 +9,20 @@ install.packages(c("data.table", "readxl", "stringr", "ggplot2", "ggrepel",
                    "ggfortify", "ggprism", "pheatmap", "VennDiagram", 
                    "corrplot", "Hmisc", "stats", "tidyverse"))
 library(data.table)
-library(edgeR)
 library(limma)
+library(edgeR)
 library(ggplot2)
 library(ggrepel)
 library(ggfortify)
 library(stats)
 library(sva)
-setwd("C:\Users\mvsan\code\YAP_TAZ_bulkRNAseq\RNAseq\Rep analysis")
+setwd("C:/users/mvsan/code/YAP_TAZ_bulkRNAseq/RNAseq/Rep analysis")
+
+##DATA CLEANING
 
 #Combine count files into single expression matrix
 # Retrieve count file paths
-file_names <- list.files(count_file_path, pattern = "featureCounts_exon\\.txt$", recursive = T, full.names = T)
+file_names <- list.files(path= ".", pattern = "featureCounts_exon\\.txt$", recursive = F, full.names = T)
 
 # Read all count files into a DGElist
 dgls <- readDGE(file_names, columns = c(1, 7), skip = 1)
@@ -31,7 +33,7 @@ sample_name <- gsub("_featureCounts_exon", "", basename(colnames(counts_raw)))
 colnames(counts_raw) <- sample_name
 
 # Save the combined count table
-fwrite(counts_raw, "counts_raw.tsv", sep = "\t")
+fwrite(counts_raw, "counts_raw.tsv", sep = "\t", row.names = T)
 
 #Reading raw count data
 count_tbl <- fread("counts_raw.tsv", data.table = F)
